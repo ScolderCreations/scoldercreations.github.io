b500a15eb2f07066aa2c59c0c2ff692df2bb79e0
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-08 15:50:56 -0700

    Update fiveyearcountdown.js

--- fiveyearcountdown.js
+++ fiveyearcountdown.js
@@ -1,7 +1,8 @@
 var fiveyears = new Date(2021, 8, 27, 16, 46);
-var showcount = setInterval(function() {
-  var current = new Date().getTime();
-  var timeleft = countDownDate - now;
+var showcount = setInterval(function) {
+  var fiveyears = new Date(2021, 8, 27, 16, 46);
+  var currentime = new Date().getTime();
+  var timeleft = fiveyears - current;
   var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
