c4a6e5d61338d690f14e3aa59fe210e220295091
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-08 15:43:03 -0700

    Update fiveyearcountdown.js

--- fiveyearcountdown.js
+++ fiveyearcountdown.js
@@ -6,16 +6,17 @@ var showcount = setInterval(function() {
   var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-}, 2000)
-document.getElementById("days").innerHTML = days + " days, "
-document.getElementById("hours").innerHTML = hours + " hours, " 
-document.getElementById("mins").innerHTML = minutes + " minutes, " 
-document.getElementById("secs").innerHTML = "and " seconds + " seconds."
-if (timeleft < 0) {
+  document.getElementById("days").innerHTML = days + " days, "
+  document.getElementById("hours").innerHTML = hours + " hours, " 
+  document.getElementById("mins").innerHTML = minutes + " minutes, " 
+  document.getElementById("secs").innerHTML = "and " seconds + " seconds."
+  if (timeleft < 0) {
     clearInterval(showcount);
     document.getElementById("days").innerHTML = ""
     document.getElementById("hours").innerHTML = "" 
     document.getElementById("mins").innerHTML = ""
     document.getElementById("secs").innerHTML = ""
     document.getElementById("result").innerHTML = "My scratch account is now at least 5 years old. Feel free to celebrate on my scratch profile.";
-}
+  }
+}, 2000)
+
